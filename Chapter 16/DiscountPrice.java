73ff58ab37c4cee4f27d992c8983cb2f60479463
Ryan Astalos <astalosrt@s.dcsdk12.org>
2018-08-31 07:37:12 -0600

    Initial Commit

---
+++ Chapter 16/DiscountPrice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DiscountPrice
+{
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        int purchases;
+        System.out.println("How Many Purchases?");
+        purchases = scan.nextInt();
+        System.out.println("Lol you bought "+ purchases +" Items?");
+
+    }
+}
---
+++ Chapter 16/OrderCheck.java
@@ -0,0 +1,7 @@
+public class OrderCheck
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
