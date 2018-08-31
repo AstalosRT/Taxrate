242c8c25410529e2ebb37a3a3a5ae1175191cd09
Ryan Astalos <astalosrt@s.dcsdk12.org>
2018-08-31 08:04:59 -0600

    Add Java file

--- Chapter 16/OrderCheck.java
+++ Chapter 16/OrderCheck.java
@@ -2,6 +2,13 @@ public class OrderCheck
 {
     public static void main(String[] args)
     {
-        
+        int bolts = 12;
+        int nuts = 8;
+        int washers = 24;
+        int cpb = 5;
+        int cpn = 3;
+        int cpw = 1;
+        int totalprice = ((bolts*cpb)+(nuts*cpn)+(washers*cpw));
+        System.out.println("The Total price is " + totalprice + " cents.");
     }
 }
